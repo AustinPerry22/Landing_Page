da00f6df8968d2c8f460a21a72ec6e22bca31bf4
Austin <austinjperry22@gmail.com>
2023-11-30 12:15:10 -0700

    removed link

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,7 +1,6 @@
 <template>
     <section class="row bg-dark">
         <div class="col-2"><button @click="scrollToComponent('home')" class="btn btn-dark">About Me</button></div>
-        <div class="col-2"><button @click="scrollToComponent('skills')" class="btn btn-dark">My Skills</button></div>
         <div class="col-2"><button @click="scrollToComponent('project-navbar')" class="btn btn-dark">My Projects</button></div>
         <div class="col-2"><button @click="scrollToComponent('contact')" class="btn btn-dark">Contact Info</button></div>
     </section>
