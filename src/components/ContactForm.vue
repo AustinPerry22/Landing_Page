c28b5e44ae809a80abdf200825cb7d87552cda27
Austin Perry <austinjperry22@gmail.com>
2024-05-01 00:53:05 -0600

    changed from web3forms to formsubmit, they have captcha built in

--- src/components/ContactForm.vue
+++ src/components/ContactForm.vue
@@ -1,8 +1,8 @@
 <template>
   <section class="row justify-content-center">
-    <form @submit="submitForm()" class="col-11 col-md-6 form bg-dark p-3 rounded-4" action="https://api.web3forms.com/submit" method="POST">
+    <form class="col-11 col-md-6 form bg-dark p-3 rounded-4" action="https://formsubmit.co/b46bd455ddbb0348650d9a29ef6935ef" method="POST">
+      <input type="text" name="_honey" style="display:none">
       <h3 class="text-center mb-3">Contact Me</h3>
-      <input type="hidden" name="access_key" value="65be978b-b4a8-479b-a49b-a55aa7dc3775">
       <!-- Form Inputs. Each input must have a name="" attribute -->
       <section class="row justify-content-center">
         <div class="col-12 col-md-6">
@@ -17,7 +17,7 @@
           <label for="message">Message:</label>
           <textarea class="form-control" name="message" type="text" maxlength="2000" rows="12" placeholder="write your message here" required></textarea>
         </div>
-        <div class="h-captcha" data-captcha="true"></div> 
+
         <button class="btn btn-success col-6 col-md-3" type="submit">Submit Form</button>
       </section>
     </form>
@@ -25,28 +25,10 @@
 </template>
 
 <script>
-import Pop from '../utils/Pop';
 
 export default {
 setup() {
   return {
-    submitForm()
-    {
-      const hCaptchaElement = document.querySelector('textarea[name="h-captcha-response"]')
-      if (!hCaptchaElement){
-        Pop.error("hCaptcha element isn't loaded in")
-        event.preventDefault()
-        return
-      }
-
-      const hCaptcha = hCaptchaElement.value
-
-      if (!hCaptcha){
-        Pop.error("Please fill out the captcha")
-        event.preventDefault()
-        return
-      }
-    }
   };
 },
 };
