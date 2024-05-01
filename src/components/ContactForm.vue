3c004cc34aa40ca9e4433d1bdcf9466b29c7e295
Austin Perry <austinjperry22@gmail.com>
2024-04-30 22:43:57 -0600

    hopeful fix for hcapcha

--- src/components/ContactForm.vue
+++ src/components/ContactForm.vue
@@ -1,6 +1,6 @@
 <template>
   <section class="row justify-content-center">
-    <form class="col-11 col-md-6 form bg-dark p-3 rounded-4" action="https://api.web3forms.com/submit" method="POST">
+    <form @submit="submitForm()" class="col-11 col-md-6 form bg-dark p-3 rounded-4" action="https://api.web3forms.com/submit" method="POST">
       <h3 class="text-center mb-3">Contact Me</h3>
       <input type="hidden" name="access_key" value="65be978b-b4a8-479b-a49b-a55aa7dc3775">
 
@@ -18,7 +18,7 @@
           <label for="message">Message:</label>
           <textarea class="form-control" name="message" type="text" maxlength="2000" rows="12" placeholder="write your message here" required></textarea>
         </div>
-        <div class="h-captcha" data-captcha="true"></div>
+        <div class="h-captcha" data-captcha="true"></div> 
         <button class="btn btn-success col-6 col-md-3" type="submit">Submit Form</button>
       </section>
     </form>
@@ -26,9 +26,29 @@
 </template>
 
 <script>
+import Pop from '../utils/Pop';
+
 export default {
 setup() {
-  return {};
+  return {
+    submitForm()
+    {
+      const hCaptchaElement = document.querySelector('textarea[name="h-captcha-response"]')
+      if (!hCaptchaElement){
+        Pop.error("hCaptcha element isn't loaded in")
+        event.preventDefault()
+        return
+      }
+
+      const hCaptcha = hCaptchaElement.value
+
+      if (!hCaptcha){
+        Pop.error("Please fill our captcha")
+        event.preventDefault()
+        return
+      }
+    }
+  };
 },
 };
 </script>
